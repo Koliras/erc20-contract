ebe49a744fb32b0a4f684d8808436dce7d32529f
Koliras <gyrmanchyk230506@gmail.com>
2024-07-24 16:55:41 +0000

    feat: add basic admin functionality

--- contracts/GooseToken.sol
+++ contracts/GooseToken.sol
@@ -7,7 +7,23 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GooseToken is ERC20 {
+    mapping(address => bool) private admins;
+
     constructor(uint256 initialSupply) ERC20("Goose", "GOS") {
         _mint(msg.sender, initialSupply);
+        admins[msg.sender] = true;
+    }
+
+    modifier isAdmin() {
+        require(admins[msg.sender]);
+        _;
+    }
+
+    function addAdmin(address newAdmin) public isAdmin {
+        admins[newAdmin] = true;
+    }
+
+    function removeAdmin(address admin) public isAdmin {
+        admins[admin] = false;
     }
 }
